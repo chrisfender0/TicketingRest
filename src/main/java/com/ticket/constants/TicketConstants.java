85bb7b39a35c7ad5fa25e21d759d234710348e83
Chris <chrisfender0@gmail.com>
2021-11-29 17:50:39 -0800

    added delete function

--- src/main/java/com/ticket/constants/TicketConstants.java
+++ src/main/java/com/ticket/constants/TicketConstants.java
@@ -11,5 +11,14 @@ public class TicketConstants {
 	 */
 	public static final String ERROR_UPDATING_TICKET = "Error updating ticket";
 	public static final String ERROR_RESOLVING_TICKET = "Error resolving ticket";
-
+	public static final String ERROR_DELETING_TICKET = "Error deleting ticket";
+	public static final String ERROR_SAVING_TICKET = "Error saving ticket";
+	/**
+	 * response messages
+	 */
+	public static final String RESPONSE = "response";
+	public static final String RESPONSE_DELETE_TICKET= "Ticket deleted successfully";
+	public static final String RESPONSE_SAVE_TICKET= "Ticket saved successfully";
+	public static final String RESPONSE_UPDATE_TICKET= "Ticket updated successfully";
+	public static final String RESPONSE_RESOLVE_TICKET= "Ticket resolved successfully";
 }
--- src/main/java/com/ticket/crudops/TicketCRUD.java
+++ src/main/java/com/ticket/crudops/TicketCRUD.java
@@ -1,7 +1,9 @@
 package com.ticket.crudops;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -18,48 +20,59 @@ public class TicketCRUD {
 		return ticketRepository.findAll();
 	}
 	
-	public static String saveNewTicket(String json, TicketRepository ticketRepository) {
+	public static Map<String, String> saveNewTicket(String json, TicketRepository ticketRepository) {
 		try {
 			Ticket ticket = TicketService.convertToTicketObject(json);
 			ticket.setDateCreated(new Date());
 			ticket.setDateModified(new Date());
 			ticketRepository.save(ticket);
-			return "Saved successfully";
+			return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.RESPONSE_SAVE_TICKET);
 		} catch(Exception e) {
 			e.printStackTrace();
-			return e.toString();
 		}
+		return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.ERROR_SAVING_TICKET);
 	}
 	
 	public static Ticket getTicketWithId(String id, TicketRepository ticketRepository) {
 		Optional<Ticket> optionalTicket =  ticketRepository.findById(TicketService.convertToId(id));
 		Ticket ticket = optionalTicket.get();
-		System.out.println(ticket.getSubject());
 		return ticket;
 	}
 	
-	public static String updateTicket(String json, TicketRepository ticketRepository) {
+	public static Map<String, String> updateTicket(String json, TicketRepository ticketRepository) {
 		try {
 			Ticket ticket = TicketService.convertToTicketObject(json);
 			ticket.setDateModified(new Date());
 			ticketRepository.save(ticket);
-			return "Ticket updated successfully";
+			return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.RESPONSE_UPDATE_TICKET);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		return TicketConstants.ERROR_UPDATING_TICKET;
+		return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.ERROR_UPDATING_TICKET);
 	}
 	
-	public static String resolveTicket(String id, TicketRepository ticketRepository){
+	public static Map<String, String> resolveTicket(String json, TicketRepository ticketRepository){
 		try {
-			Ticket ticket = getTicketWithId(id, ticketRepository);
+			Ticket ticket = TicketService.convertToTicketObject(json);
 			ticket.setStatus(TicketConstants.STATUS_RESOLVED);
 			ticket.setDateModified(new Date());
 			ticketRepository.save(ticket);
-			return "Ticket resolved successfully";
+			return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.RESPONSE_RESOLVE_TICKET);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return TicketConstants.ERROR_RESOLVING_TICKET;
+		return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.ERROR_RESOLVING_TICKET);
 	}
+	
+	public static Map<String, String> deleteTicket(String json, TicketRepository ticketRepository){
+		try {
+			Ticket ticket = TicketService.convertToTicketObject(json);
+			ticketRepository.delete(ticket);
+			return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.RESPONSE_DELETE_TICKET);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Collections.singletonMap(TicketConstants.RESPONSE, TicketConstants.ERROR_DELETING_TICKET);
+	}
+	
 }
